893360a7e39a90f1e5dc3535ee1e636319dad345
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-11 13:59:04 -0700

    notes updates

---
+++ Strings.cs
@@ -0,0 +1,5 @@
+// See https://aka.ms/new-console-template for more information
+// string firstName = "John ";
+// string lastName = "Doe";
+// string name = firstName + lastName;
+// Console.WriteLine(name);
